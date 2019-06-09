31dd14e579dfee7b631ce53a04e8ff98e42fbe9b
WendySanarwanto <wendy.sanarwanto@gmail.com>
2019-06-09 17:10:32 +0700

    renamed introductionary main.go

---
+++ main.intro.go
@@ -0,0 +1,37 @@
+package main
+
+// import "fmt"
+
+// var aNumber int
+
+// func main() {
+// 	// Declaring a string variable in go
+// 	var aceSpadeCard string = newCard()
+
+// 	// Alternative way to declare and assign a variable
+// 	heartCard := "Ace of Hearts"
+// 	fmt.Println(heartCard)
+
+// 	// Re-assigning a new value to the heartCard
+// 	heartCard = "Five of Hearts"
+
+// 	// Integer var
+// 	// var aNumber int
+// 	aNumber = 10
+
+// 	fmt.Println(aceSpadeCard)
+// 	fmt.Println(heartCard)
+// 	fmt.Println(aNumber)
+
+// 	// Declaring a slice (resizable array)
+// 	cards := deck { aceSpadeCard, heartCard }
+
+// 	// Append a new item into the slice
+// 	cards = append(cards, "Nine of Diamonds")
+
+// 	cards.print()
+// }
+
+// func newCard() string {
+// 	return "Ace of Spades"
+// }
